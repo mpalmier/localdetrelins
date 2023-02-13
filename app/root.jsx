8dabcc1dfa43c82afb703c2a02fed7ebaae463d4
mpalmier <mat42130@hotmail.fr>
2023-02-13 11:40:20 +0100

    [*] init repo

---
+++ app/root.jsx
@@ -0,0 +1,49 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from '@remix-run/react';
+import styles from './styles/app.css';
+import favicon from '../public/favicon.svg';
+import {Layout} from './components/Layout';
+
+export const meta = () => ({
+  charset: 'utf-8',
+  viewport: 'width=device-width,initial-scale=1',
+});
+export async function loader({context}) {
+  const layout = await context.storefront.query(LAYOUT_QUERY);
+  return {layout};
+}
+export default function App() {
+  const data = useLoaderData();
+
+  const {name} = data.layout.shop;
+
+  return (
+      <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+      <Layout title={name}>
+        <Outlet />
+      </Layout>
+      <ScrollRestoration />
+      <Scripts />
+      </body>
+      </html>
+  );
+}
+const LAYOUT_QUERY = `#graphql
+  query layout {
+    shop {
+      name
+      description
+    }
+  }
+`;
